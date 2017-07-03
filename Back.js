014dbcbf7cf3f796b9164fd3e64f0881182938ca
arvindkalra <kalarvind97@gmail.com>
2017-07-04 00:12:14 +0530

    Adding

---
+++ Back.js
@@ -0,0 +1,16 @@
+/**
+ * Created by arvind on 3/7/17.
+ */
+// $(function () {
+//     var out = $('#out');
+//     $.ajax({url: "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=56e3159a16df4273aff81748170307&q=Mumbai&format=json&num_of_days=5&fx=yes", success: function(result){
+//         console.log(result);
+//         $("#div1").html(result);
+//
+//     }});
+// /*
+// });*/
+    $(window).load(function() {
+        $('#body').css({'display':'block'});
+        $(".loader").css({'display':'none'});
+    });
